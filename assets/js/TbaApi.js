fb52f3289e85bf326f0515ebad3556695d97ea19
kenschenke <kenschenke@gmail.com>
2023-01-15 19:54:10 +0000

    Deploying to gh-pages from @ FRCTeam1987/teambroncobots.com@e033f3399830fadae74752fe8c991810328bafee 🚀

---
+++ assets/js/TbaApi.js
@@ -0,0 +1,168 @@
+const tbaUrl = "https://www.thebluealliance.com/api/v3";
+const apiKey = "Not Here";
+let requestCounter = 0;
+let timeWaiting = 0; //in seconds
+let error = "";
+function getStatus() {
+    requestCounter++;
+    return $.ajax({
+        method: "GET",
+        url: tbaUrl + "/status",
+        data: {},
+        headers: {
+            "accept": "application/json",
+            "X-TBA-Auth-Key": apiKey
+        },
+        success: function(response) {
+            requestCounter--;
+            if (requestCounter === 0) {
+                isFinished()
+            }
+        },
+        error: function () {
+            requestCounter = -1;
+            isFinished();
+        }
+    });
+}
+
+function getEvents() {
+    requestCounter++;
+    return $.ajax({
+        method: "GET",
+        url: tbaUrl + "/team/frc1987/events/simple",
+        data: {},
+        headers: {
+            "accept": "application/json",
+            "X-TBA-Auth-Key": apiKey
+        },
+        success: function(response) {
+            requestCounter--;
+            if (requestCounter === 0) {
+                isFinished()
+            }
+        }
+    });
+}
+
+function getYearsParticipated() {
+    requestCounter++;
+    return $.ajax({
+        method: "GET",
+        url: tbaUrl + "/team/frc1987/years_participated",
+        data: {},
+        headers: {
+            "accept": "application/json",
+            "X-TBA-Auth-Key": apiKey
+        },
+        success: function(response) {
+            requestCounter--;
+            if (requestCounter === 0) {
+                isFinished()
+            }
+        }
+    });
+}
+
+function getEventsPerYear(year) {
+    requestCounter++;
+    return $.ajax({
+        method: "GET",
+        url: tbaUrl + "/team/frc1987/events/" + year + "/simple",
+        data: {},
+        headers: {
+            "accept": "application/json",
+            "X-TBA-Auth-Key": apiKey
+        },
+        success: function(response) {
+            requestCounter--;
+            if (requestCounter === 0) {
+                isFinished()
+            }
+        }
+    });
+}
+
+function getAwardsPerYear(year) {
+    requestCounter++;
+    return $.ajax({
+        method: "GET",
+        url: tbaUrl + "/team/frc1987/awards/" + year,
+        data: {},
+        headers: {
+            "accept": "application/json",
+            "X-TBA-Auth-Key": apiKey
+        },
+        success: function(response) {
+            requestCounter--;
+            if (requestCounter === 0) {
+                isFinished()
+            }
+        }
+    });
+}
+
+function years() {
+    requestCounter++;
+    return $.ajax({
+        method: "GET",
+        url: tbaUrl + "/team/frc1987/years_participated",
+        data: {},
+        headers: {
+            "accept": "application/json",
+            "X-TBA-Auth-Key": apiKey
+        },
+        success: function(response) {
+            requestCounter--;
+            if (requestCounter === 0) {
+                isFinished()
+            }
+        }
+    });
+}
+
+function getMatchesPerYear(year) {
+    requestCounter++;
+    return $.ajax({
+        method: "GET",
+        url: tbaUrl + "/team/frc1987/matches/" + year + "/simple",
+        data: {},
+        headers: {
+            "accept": "application/json",
+            "X-TBA-Auth-Key": apiKey
+        },
+        success: function(response) {
+            requestCounter--;
+            if (requestCounter === 0) {
+                isFinished()
+            }
+        }
+    });
+}
+async function getRequestCounter() {
+    return requestCounter;
+}
+async function getError() {
+    return error;
+}
+async function isFinished() {
+    while (requestCounter > 0) {
+        if (requestCounter === -1) {
+            error = "The Blue Alliance Appears to be down.";
+            return false;
+        }
+        console.log("There are ${requestCounter} active call(s). Waiting...");
+        await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 second
+        timeWaiting++; //in seconds
+        if (timeWaiting > 15) {
+            console.log(`There are ${requestCounter} active call(s). Timed Out.`);
+            error = `There are ${requestCounter} active call(s). Timed Out.`;
+            return false;
+        }
+    }
+    if (requestCounter === 0) {
+        error = "";
+        return true;
+    }
+}
+
