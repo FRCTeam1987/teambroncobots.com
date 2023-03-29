affb8d5675d0c6a9fc2a9a8e3c7bfc9d82c5bca2
kenschenke <kenschenke@gmail.com>
2023-03-29 16:52:42 +0000

    Deploying to gh-pages from @ FRCTeam1987/teambroncobots.com@5640c04b2ba5c240f60dd0895d5750fe807f3ee7 🚀

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-//
+function isOnMobileDevice() {
+    let isMobile = navigator.userAgentData.mobile;
+    return isMobile;
+}
+
+function getSeasonsCompeted() {
+    let currentDate = new Date();
+    let year2007 = new Date(2007, 1, 7); //Jan 1 2007
+    let timeDifference = currentDate.getTime() - year2007.getTime();
+    let yearsDifference = timeDifference / 31536000000; //number of milliseconds in a year
+    return Math.floor(yearsDifference);
+}
