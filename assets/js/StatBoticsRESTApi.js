f90d1e42f0510dad90e4fa7f86c5bc38e834837a
Jon Smith <3228373+j0n5m1th@users.noreply.github.com>
2026-09-06 15:59:24 -0500

    fix link / rankings display

--- assets/js/StatBoticsRESTApi.js
+++ assets/js/StatBoticsRESTApi.js
@@ -341,10 +341,10 @@ async function scoreData(year, isRobotsPage) {
             (winrate * 100).toFixed(2) + '% Win Rate<br/>' +
             percentileFormatted + ordinalSuffix + " percentile<br/><br/>" +
             '</div>' +
-            '<div class="column is-6" style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center;">' +
-            '<div style="font-size: 1em; margin-right: 2.5rem; margin-bottom: 1rem; width: 6.5em; height: 6.5em; background-color: #990202; border-radius: 24%; color: #F2F2F2; display: flex; justify-content: center; align-items: center;">Ranked<br/>' + state_epa_rank + '/' + state_team_count + '<br/>in Missouri</div>' +
-            '<div style="font-size: 1em; margin-right: 2.5rem; margin-bottom: 1rem; width: 6.5em; height: 6.5em; background-color: #830506; border-radius: 24%; color: #F2F2F2; display: flex; justify-content: center; align-items: center;">Ranked<br/>' + country_epa_rank + '/' + country_team_count + '<br/>in the USA</div>' +
-            '<div style="font-size: 1em; margin-bottom: 1rem; width: 6.5em; height: 6.5em; background-color: #990202; border-radius: 24%; color: #F2F2F2; display: flex; justify-content: center; align-items: center;">Ranked<br/>' + total_epa_rank + '/' + total_team_count + '<br/>in the world</div>' +
+            '<div class="column is-6" style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center; gap: 1rem 2.5rem;">' +
+            '<div style="font-size: 1em; width: 6.5em; height: 6.5em; background-color: #830506; border-radius: 24%; color: #F2F2F2; display: flex; justify-content: center; align-items: center;">Ranked<br/>' + state_epa_rank + '/' + state_team_count + '<br/>in Missouri</div>' +
+            '<div style="font-size: 1em; width: 6.5em; height: 6.5em; background-color: #830506; border-radius: 24%; color: #F2F2F2; display: flex; justify-content: center; align-items: center;">Ranked<br/>' + country_epa_rank + '/' + country_team_count + '<br/>in the USA</div>' +
+            '<div style="font-size: 1em; width: 6.5em; height: 6.5em; background-color: #830506; border-radius: 24%; color: #F2F2F2; display: flex; justify-content: center; align-items: center;">Ranked<br/>' + total_epa_rank + '/' + total_team_count + '<br/>in the world</div>' +
             '</div>' +
             '</div>';
     } else {
